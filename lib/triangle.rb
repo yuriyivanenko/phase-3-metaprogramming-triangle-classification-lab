28d23adf22952d2c8364077dd58dfc4edbc383d9
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-11 10:55:30 -0400

    Pass All Tests

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    if a == 0 && b == 0 && c ==0
+      raise TriangleError
+    elsif a < 0 || b < 0 || c < 0
+      raise TriangleError
+    elsif a + b <= c || c + b <= a || a + c <= b
+      raise TriangleError
+    else
+      @a = a
+      @b = b
+      @c = c
+    end
+  end
+
+  def kind
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
